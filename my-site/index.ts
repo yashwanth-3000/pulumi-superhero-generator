0fd930dd3adaf25046e7e26db6e66f2bd96173c3
Yashwanth Krishna <yashwanthkrishna@Yashwanths-Mac-mini.local>
2025-04-07 00:56:46 +0530

    Final

--- my-site/index.ts
+++ my-site/index.ts
@@ -66,9 +66,63 @@ const bucketFolder = new synced_folder.S3BucketFolder("bucket-folder", {
     acl: "public-read",
 }, { dependsOn: [ownershipControls, publicAccessBlock, bucketPolicy] });
 
+// Create a response headers policy for CloudFront
+const responseHeadersPolicy = new aws.cloudfront.ResponseHeadersPolicy("cors-headers-policy", {
+    corsConfig: {
+        accessControlAllowCredentials: false,
+        accessControlAllowHeaders: {
+            items: ["*"],
+        },
+        accessControlAllowMethods: {
+            items: ["GET", "HEAD", "OPTIONS"],
+        },
+        accessControlAllowOrigins: {
+            items: ["*"],
+        },
+        accessControlMaxAgeSec: 600,
+        originOverride: true,
+    },
+    customHeadersConfig: {
+        items: [
+            // Remove X-Content-Type-Options as it's a security header
+            // and should be handled in securityHeadersConfig
+        ],
+    },
+    securityHeadersConfig: {
+        contentSecurityPolicy: {
+            contentSecurityPolicy: "default-src 'self' https: data: 'unsafe-inline' 'unsafe-eval'; img-src 'self' data: https:;",
+            override: true,
+        },
+        frameOptions: {
+            frameOption: "SAMEORIGIN",
+            override: true,
+        },
+        strictTransportSecurity: {
+            accessControlMaxAgeSec: 63072000,
+            includeSubdomains: true,
+            override: true,
+            preload: true,
+        },
+        xssProtection: {
+            override: true,
+            protection: true,
+        },
+        // Add X-Content-Type-Options in the security headers config
+        contentTypeOptions: {
+            override: true,
+        },
+    },
+});
+
+// Create AWS managed policies for CloudFront
+const cachingOptimizedPolicy = "658327ea-f89d-4fab-a63d-7e88639e58f6"; // CachingOptimized ID 
+const CORSPolicy = "88a5eaf4-2fd4-4709-b370-b4c650ea3fcf"; // CORS-S3Origin policy ID
+const allViewerExceptHostHeaderPolicy = "b689b0a8-53d0-40ab-baf2-68738e2966ac"; // AllViewerExceptHostHeader policy ID
+
 // Create a CloudFront CDN to distribute and cache the website.
 const cdn = new aws.cloudfront.Distribution("dynamicui-cdn", {
     enabled: true,
+    defaultRootObject: indexDocument,
     origins: [{
         originId: bucket.arn,
         domainName: bucketWebsite.websiteEndpoint,
@@ -77,11 +131,13 @@ const cdn = new aws.cloudfront.Distribution("dynamicui-cdn", {
             httpPort: 80,
             httpsPort: 443,
             originSslProtocols: ["TLSv1.2"],
+            originReadTimeout: 60,
+            originKeepaliveTimeout: 60,
         },
     }],
     defaultCacheBehavior: {
         targetOriginId: bucket.arn,
-        viewerProtocolPolicy: "redirect-to-https",
+        viewerProtocolPolicy: "allow-all",
         allowedMethods: [
             "GET",
             "HEAD",
@@ -92,22 +148,49 @@ const cdn = new aws.cloudfront.Distribution("dynamicui-cdn", {
             "HEAD",
             "OPTIONS",
         ],
-        defaultTtl: 600,
-        maxTtl: 600,
-        minTtl: 600,
+        // Use cache policy instead of forwardedValues (which is deprecated)
+        cachePolicyId: cachingOptimizedPolicy,
+        originRequestPolicyId: allViewerExceptHostHeaderPolicy,
+        // Comment out forwardedValues as it's deprecated
+        /*
         forwardedValues: {
             queryString: true,
             cookies: {
                 forward: "all",
             },
+            // Don't forward the Host header to S3
+            headers: ["Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"],
         },
+        */
+        responseHeadersPolicyId: responseHeadersPolicy.id,
     },
     priceClass: "PriceClass_100",
-    customErrorResponses: [{
-        errorCode: 404,
-        responseCode: 200,
-        responsePagePath: `/${indexDocument}`,
-    }],
+    customErrorResponses: [
+        {
+            errorCode: 404,
+            responseCode: 200,
+            responsePagePath: `/${indexDocument}`,
+            errorCachingMinTtl: 0,  // Don't cache errors
+        },
+        {
+            errorCode: 403,
+            responseCode: 200,
+            responsePagePath: `/${indexDocument}`,
+            errorCachingMinTtl: 0,  // Don't cache errors
+        },
+        {
+            errorCode: 500,
+            responseCode: 200,
+            responsePagePath: `/${indexDocument}`,
+            errorCachingMinTtl: 0,  // Don't cache errors
+        },
+        {
+            errorCode: 503,
+            responseCode: 200,
+            responsePagePath: `/${indexDocument}`,
+            errorCachingMinTtl: 0,  // Don't cache errors
+        }
+    ],
     restrictions: {
         geoRestriction: {
             restrictionType: "none",
@@ -122,3 +205,7 @@ const cdn = new aws.cloudfront.Distribution("dynamicui-cdn", {
 export const websiteUrl = pulumi.interpolate`http://${bucketWebsite.websiteEndpoint}`;
 export const cdnUrl = pulumi.interpolate`https://${cdn.domainName}`;
 export const cdnHostname = cdn.domainName;
+
+// Note: After deploying, you may need to manually invalidate the CloudFront cache 
+// using the AWS CLI or Console to see HTML changes immediately:
+// aws cloudfront create-invalidation --distribution-id <DISTRIBUTION_ID> --paths "/*"
